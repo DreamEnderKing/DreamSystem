fb1fa4c90f70b9b01646e1cd64f0ecd6be54edc2
David Zhang <www20040718@163.com>
2019-11-24 05:42:06 +0000

    Updated 5 files under /WindowsFormsApp1

---
+++ WindowsFormsApp1/StartManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace WindowsFormsApp1
+{
+    public partial class StartManager : Form
+    {
+        public StartManager()
+        {
+            InitializeComponent();
+        }
+
+        //获取开机秒数
+        int time = 0;
+        private void getTime()
+        {
+            XmlDocument xml = new XmlDocument();
+            xml.Load(Application.StartupPath + "\\PC\\start\\start.xml");
+            XmlElement element = (XmlElement)xml.SelectSingleNode("start\\configure\\time");
+            time = element.GetAttribute("value").Length;
+            MessageBox.Show(time.ToString());
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void StartManager_Load(object sender, EventArgs e)
+        {
+            getTime();
+        }
+    }
+}
